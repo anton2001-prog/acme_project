f3359bbb1705f9676734ee5bcde8114302e2feb2
Антон Никифоров <anton2001@mail.ru>
2024-01-29 21:50:41 +0300

    homework1

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,21 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = [
+        'first_name',
+        'last_name',
+        'birthday'
+    ]
+    list_editable = [
+        'first_name',
+        'last_name',
+        'birthday'
+    ]
+    list_display_links = list_display = [
+        'first_name'
+    ]
+
+    admin.site.register(Birthday)
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.urls import reverse
+
+from .validators import real_age
 
 
 class Birthday(models.Model):
@@ -12,4 +15,23 @@ class Birthday(models.Model):
         verbose_name='Фамилия',
         help_text='Необязательное поле'
     )
-    birthday = models.DateField(verbose_name='Дата рождения')
+    birthday = models.DateField(
+        verbose_name='Дата рождения',
+        validators=(real_age,)
+    )
+    image = models.ImageField(
+        verbose_name='Фото',
+        upload_to='birthdays_images',
+        blank=True
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
+
+    def get_absolute_url(self):
+        return reverse("birthday:detail", kwargs={"pk": self.pk})
